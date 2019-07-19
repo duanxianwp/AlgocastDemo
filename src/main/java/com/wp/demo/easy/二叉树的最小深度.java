5374cb666d5df59b5b1a64c1ab142e89a14276dd
duanxianwp <765382553@qq.com>
2019-07-19 18:47:57 +0800

    fix

--- src/main/java/com/wp/demo/easy/二叉树的最小深度.java
+++ src/main/java/com/wp/demo/easy/二叉树的最小深度.java
@@ -29,6 +29,12 @@ public class 二叉树的最小深度 {
         if (root.left == null && root.right == null) {
             return 1;
         }
+        if (root.left == null){
+            return minDepth(root.right)+1;
+        }
+        if (root.right == null){
+            return minDepth(root.left)+1;
+        }
         return Math.min(minDepth(root.left), minDepth(root.right)) + 1;
     }
 
